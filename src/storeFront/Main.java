7b9403de818c18539d75ea513c9820e48f248702
mick2j <magrove2j@gmail.com>
2022-10-25 17:47:19 -0700

    store front project

---
+++ src/storeFront/Main.java
@@ -0,0 +1,40 @@
+package storeFront;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int department = 0;
+		
+	
+		
+		System.out.println("*****************************************************************************");
+		System.out.println("***********************WELCOME TO OUR STORE**********************************");
+		System.out.println("*****************************************************************************");
+		System.out.println(" Our store features a variety of items from different departments\n");
+		
+		
+		
+			System.out.println(" Which department would you like to enter?");
+			System.out.println(" 1 Men's Department\n");
+			System.out.println(" 2 Women's Department\n");
+			System.out.println(" 3 Home/Technology Department\n");
+			System.out.println(" 4 Boys Clothing Department\n");
+			System.out.println(" 5 Girls Clothing Department\n");
+		 
+			department = Integer.parseInt(in.nextLine());
+			
+			switch(department) {
+			case 1:
+				// display department
+				System.out.println(" Men's Department");
+				System.out.println(" Here are some featured items");
+				//display items 
+				
+				
+				
+			}
+		
+	}
+
+}
